d6816ac69a980d620c28d525c008a825cc8c3fb1
zahid.hasan <zahid.hasan@dreamonline.co.jp>
2023-10-11 17:39:49 +0600

    database connection

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello From Expense Tracker Backend!!")
+require('dotenv').config()
+
+const express = require('express')
+
+const port = process.env.PORT
+
+const { dbConnection } = require('./config/db.config')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true } ))
+
+dbConnection()
+
+app.get('/', (req, res) => {
+    res.send('Hello from expense tracker backend!')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on 127.0.0.1:${port}`)
+})
