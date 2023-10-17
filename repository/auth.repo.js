679f5867278602db0282278e162f8c384f8e8232
DESKTOP-3LC3JT7\Zahid Hasan <zahidcse4960@gmail.com>
2023-10-18 00:56:45 +0600

    Working on API development of user registration & login

--- repository/auth.repo.js
+++ repository/auth.repo.js
@@ -1,6 +1,11 @@
 const UserModel = require('../model/user.model')
 
 
+/**
+ * function to check existance of user by email
+ * @param {*} email 
+ * @returns true || false
+ */
 exports.isUserExistOrNotByEmail = async (email) => {
     const user = await UserModel.findOne({ email: email })
     return user !== null
