ca6e9e310be8c56097023eba17b16359595cdfa7
zahid.hasan <zahid.hasan@dreamonline.co.jp>
2023-10-31 16:43:29 +0600

    Writing swagger definition for expense category feature.

--- schema/expense-category.schema.js
+++ schema/expense-category.schema.js
@@ -6,7 +6,7 @@ const { Schema } = require('mongoose')
  */
 const expenseCategorySchema = new Schema({
     categoryName: { type: String, required: true },
-    description: { type: String, required: true},
+    categoryDescription: { type: String, required: true},
     isActive: { type: Boolean, default: true },
     createdAt: { type: Date, default: Date.now() }
 })
