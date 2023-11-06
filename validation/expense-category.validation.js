f9cad81d27fa695751c9ece8990be21e4c94e347
zahid.hasan <zahid.hasan@dreamonline.co.jp>
2023-11-06 10:54:03 +0600

    developed api [patch]/api/user/{userId}/expense-categories/{expenseCategoryId}

--- validation/expense-category.validation.js
+++ validation/expense-category.validation.js
@@ -8,3 +8,13 @@ exports.addExpenseCategoryValidationSchema = joi.object({
     categoryName: joi.string().required(),
     categoryDescription: joi.string().required()
 })
+
+
+/**
+ * validation schema to update expense category
+ */
+exports.updateExpenseCategoryValidationSchema = joi.object({
+    categoryName: joi.string().required(),
+    categoryDescription: joi.string().required(),
+    isActive: joi.boolean().required()
+})
